3809ffa27d2e25c2184a8082ca0cc25ae1b03534
Bartosz <bartosz.kwec@op.pl>
2021-01-24 00:22:04 +0100

    11

--- android/app/src/main/kotlin/pl/kwec/fire_station_inz_app/MainActivity.kt
+++ android/app/src/main/kotlin/pl/kwec/fire_station_inz_app/MainActivity.kt
@@ -1,6 +1,11 @@
 package pl.kwec.fire_station_inz_app
 
+
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
 
 class MainActivity: FlutterActivity() {
 }
